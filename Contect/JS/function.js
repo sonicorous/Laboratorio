8a627b12089596c2e009261654147def80e6a685
Luis Angel <lagarcia@weecompany.net>
2023-02-22 23:07:45 -0600

    Ser siempre el mejor

--- Contect/JS/function.js
+++ Contect/JS/function.js
@@ -47,164 +47,99 @@ slider.addEventListener('mousemove', e => {
     slider.scrollLeft = scrollLeft - walk;
 });
 
-
-var langs =
-[['Afrikaans',       ['af-ZA']],
- ['Bahasa Indonesia',['id-ID']],
- ['Bahasa Melayu',   ['ms-MY']],
- ['Català',          ['ca-ES']],
- ['Čeština',         ['cs-CZ']],
- ['Deutsch',         ['de-DE']],
- ['English',         ['en-AU', 'Australia'],
-                     ['en-CA', 'Canada'],
-                     ['en-IN', 'India'],
-                     ['en-NZ', 'New Zealand'],
-                     ['en-ZA', 'South Africa'],
-                     ['en-GB', 'United Kingdom'],
-                     ['en-US', 'United States']],
- ['Español',         ['es-AR', 'Argentina'],
-                     ['es-BO', 'Bolivia'],
-                     ['es-CL', 'Chile'],
-                     ['es-CO', 'Colombia'],
-                     ['es-CR', 'Costa Rica'],
-                     ['es-EC', 'Ecuador'],
-                     ['es-SV', 'El Salvador'],
-                     ['es-ES', 'España'],
-                     ['es-US', 'Estados Unidos'],
-                     ['es-GT', 'Guatemala'],
-                     ['es-HN', 'Honduras'],
-                     ['es-MX', 'México'],
-                     ['es-NI', 'Nicaragua'],
-                     ['es-PA', 'Panamá'],
-                     ['es-PY', 'Paraguay'],
-                     ['es-PE', 'Perú'],
-                     ['es-PR', 'Puerto Rico'],
-                     ['es-DO', 'República Dominicana'],
-                     ['es-UY', 'Uruguay'],
-                     ['es-VE', 'Venezuela']],
- ['Euskara',         ['eu-ES']],
- ['Français',        ['fr-FR']],
- ['Galego',          ['gl-ES']],
- ['Hrvatski',        ['hr_HR']],
- ['IsiZulu',         ['zu-ZA']],
- ['Íslenska',        ['is-IS']],
- ['Italiano',        ['it-IT', 'Italia'],
-                     ['it-CH', 'Svizzera']],
- ['Magyar',          ['hu-HU']],
- ['Nederlands',      ['nl-NL']],
- ['Norsk bokmål',    ['nb-NO']],
- ['Polski',          ['pl-PL']],
- ['Português',       ['pt-BR', 'Brasil'],
-                     ['pt-PT', 'Portugal']],
- ['Română',          ['ro-RO']],
- ['Slovenčina',      ['sk-SK']],
- ['Suomi',           ['fi-FI']],
- ['Svenska',         ['sv-SE']],
- ['Türkçe',          ['tr-TR']],
- ['български',       ['bg-BG']],
- ['Pусский',         ['ru-RU']],
- ['Српски',          ['sr-RS']],
- ['한국어',            ['ko-KR']],
- ['中文',             ['cmn-Hans-CN', '普通话 (中国大陆)'],
-                     ['cmn-Hans-HK', '普通话 (香港)'],
-                     ['cmn-Hant-TW', '中文 (台灣)'],
-                     ['yue-Hant-HK', '粵語 (香港)']],
- ['日本語',           ['ja-JP']],
- ['Lingua latīna',   ['la']]];
-
-let select_language = document.querySelector('#select_language');
-let select_dialect = document.querySelector('#select_dialect');
-
-for (var i = 0; i < langs.length; i++) {
-  select_language.options[i] = new Option(langs[i][0], i);
-}
-
-select_language.selectedIndex = 7;
-updateCountry();
-select_dialect.selectedIndex = 11;
-
-function updateCountry() {
-    for (var i = select_dialect.options.length - 1; i >= 0; i--) {
-        select_dialect.remove(i);
-    }
-    var list = langs[select_language.selectedIndex];
-    for (var i = 1; i < list.length; i++) {
-        select_dialect.options.add(new Option(list[i][1], list[i][0]));
-    }
-    select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
-}
+let Comandos = [
+    { Pregunta:'hola',Respuesta:'Hola, en que te puedo ayudar!'},
+    { Pregunta:'guardar ejecutar',Respuesta:'Que deseas guardar?!'},
+    { Pregunta:'ayudar ayuda',Respuesta:'En que te puedo ayudar?!'}
+];
 
 if ("webkitSpeechRecognition" in window) {
     let speechRecognition = new webkitSpeechRecognition();
-    let final_transcript = "";
-
     speechRecognition.continuous = true;
     speechRecognition.interimResults = true;
-    speechRecognition.lang = document.querySelector("#select_dialect").value;
+    speechRecognition.lang = "es-MX";
 
     speechRecognition.onstart = () => {
-        document.querySelector("#status").style.display = "block";
+        $("#status").show();
+        speak('En que te puedo ayudar?')
     };
+
     speechRecognition.onerror = () => {
-        document.querySelector("#status").style.display = "none";
-        console.log("Speech Recognition Error");
+        $("#status").hide();
+        speak('Adios')
+        alert("Speech Recognition Error");
     };
+
     speechRecognition.onend = () => {
-        document.querySelector("#status").style.display = "none";
+        $("#status").hide();
+        speak('Adios');
+        $("#final").html('<div class="item-conversacion">Texto</div>');
         console.log("Speech Recognition Ended");
     };
 
     speechRecognition.onresult = (event) => {
         let interim_transcript = "";
-
+        let final_transcript = "";
         for (let i = event.resultIndex; i < event.results.length; ++i) {
-        if (event.results[i].isFinal) {
-            final_transcript += event.results[i][0].transcript;
-        } else {
-            interim_transcript += event.results[i][0].transcript;
+            if (event.results[i].isFinal) {
+                final_transcript += event.results[i][0].transcript;
+            } else {
+                interim_transcript += event.results[i][0].transcript;
+            } 
         }
+
+        if(final_transcript.trim() != ""){
+            $("#final").append(`<div class="item-conversacion">${final_transcript}</div>`);
+
+            let FindComandos = fnc_BusquedaPregunta(final_transcript.replace('.','').toLowerCase())
+            if(FindComandos.length > 0){
+                speak(FindComandos[0].Respuesta);
+            } else {
+                speak('Ok Adios!');
+            }
         }
-        document.querySelector("#final").innerHTML = final_transcript;
-        document.querySelector("#interim").innerHTML = interim_transcript;
+        
     };
 
-    document.querySelector("#start").onclick = () => {
+    $("#start").on('click',function(){
         speechRecognition.start();
-    };
-    document.querySelector("#stop").onclick = () => {
+    });
+
+    $("#stop").on('click',function(){
         speechRecognition.stop();
-    };
+    });
 } else {
     alert("Speech Recognition Not Available");
 }
 
-var supportMsg = document.getElementById('msg');
-
-if ('speechSynthesis' in window) {
-	supportMsg.innerHTML = 'Your browser <strong>supports</strong> speech synthesis.';
-} else {
-	supportMsg.innerHTML = 'Sorry your browser <strong>does not support</strong> speech synthesis.<br>Try this in <a href="https://www.google.co.uk/intl/en/chrome/browser/canary.html">Chrome Canary</a>.';
-	supportMsg.classList.add('not-supported');
+function fnc_BusquedaPregunta(Comando) {
+    let FindComandos = Comandos.filter(function(value) {
+        return value.Pregunta.includes(Comando); 
+    });
+    console.log(FindComandos.length)
+    return FindComandos;
 }
 
-
-var button = document.getElementById('speak');
-var speechMsgInput = document.getElementById('speech-msg');
 function speak(text) {
 	var msg = new SpeechSynthesisUtterance();
 	msg.text = text;
 	msg.volume = 1;
 	msg.rate = 1;
 	msg.pitch = 1;
-    msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == "Microsoft Sabina - Spanish (Mexico)"; })[0];
+    msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == "Microsoft LorenzoEsCL Online (Natural) - Spanish (Mexico)"; })[0];
 	
 	window.speechSynthesis.speak(msg);
 }
 
-button.addEventListener('click', function(e) {
-	if (speechMsgInput.value.length > 0) {
-		speak(speechMsgInput.value);
+$("#speak").on('click',function(){
+    let speechMsgInput = $('#speech-msg').val();
+	if (speechMsgInput.length > 0) {
+		speak(speechMsgInput);
 	}
 });
+
+
+
+
 
 //https://codepen.io/denic/pen/GRoOxbM
